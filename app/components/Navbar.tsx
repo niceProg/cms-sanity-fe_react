57ade13689965a9d59754b58fa8f5af6701e6b86
niceProg <wisnu.yudhanta@gmail.com>
2024-10-04 02:04:11 +0700

    fresh

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,7 +3,6 @@ import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import { LNavbar } from "./Logo";
 import { Bars3BottomRightIcon, XMarkIcon } from "@heroicons/react/24/solid";
-import "@/app/styles/blog_content.scss";
 
 type MenuItem = {
      title: string;
